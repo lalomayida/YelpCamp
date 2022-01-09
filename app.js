13f3384f71beaf83f86dd4dbf5dc142bc9e7d20d
Luis Eduardo Mayida González <51863317+lalomayida@users.noreply.github.com>
2022-01-08 20:11:25 -0600

    Adding layouts

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
-const campground = require("./models/campground");
-
+const methodOverride = require("method-override");
+const engine = require("ejs-mate");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,11 +17,14 @@ db.once("open", () => {
 });
 
 const app = express();
+app.engine("ejs", engine);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); // For express to be able to parse jsons into the body :)
+
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -48,6 +51,27 @@ app.get("/campgrounds/:id", async (req, res) => {
   res.render("campgrounds/show", { campground });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const id = req.params.id;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
