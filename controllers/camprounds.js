5b1f5933a42ebe3a8eaedf5903d7521572ca2441
Luis Eduardo Mayida González <51863317+lalomayida@users.noreply.github.com>
2022-01-13 22:08:53 -0600

    Solving index problem wiht images

--- controllers/camprounds.js
+++ controllers/camprounds.js
@@ -17,6 +17,7 @@ module.exports.createCampground = async (req, res, next) => {
   }));
   campground.author = req.user._id;
   await campground.save();
+  console.log(campground.images);
   req.flash("success", "Successfully made a campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
